5d26abf65a7830efa42d8ef34c82e2109cac8016
Ilya Kirienkov <garsiya@gmail.com>
2016-12-03 17:39:44 +0300

    Первоначальный коммит

---
+++ answers.py
@@ -0,0 +1,12 @@
+def get_answer(k, d):
+    return d.get(k)
+
+
+dialog = {"Привет": "И тебе привет", "Как дела?": "Лучше всех", "Пока": "Увидимся"}
+key = input("Введите ключ ")
+print(get_answer(key.capitalize(), dialog))
+
+
+
+
+git
---
+++ info.py
@@ -0,0 +1,8 @@
+user_info = {}
+user_info = {"first_name":"","last_name":""}
+f_name = input("Введите ваше имя: ")
+user_info["first_name"] = f_name
+l_name = input("Введите фамилию: ")
+user_info["last_name"] = l_name
+print(user_info)
+
