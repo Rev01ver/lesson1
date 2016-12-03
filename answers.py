fa3992e1e7fde94bc086f94e83e87e7d0cdd5e60
Ilya Kirienkov <garsiya@gmail.com>
2016-12-03 17:56:25 +0300

    убран мусор

--- answers.py
+++ answers.py
@@ -1,12 +1,5 @@
 def get_answer(k, d):
     return d.get(k)
-
-
 dialog = {"Привет": "И тебе привет", "Как дела?": "Лучше всех", "Пока": "Увидимся"}
 key = input("Введите ключ ")
 print(get_answer(key.capitalize(), dialog))
-
-
-
-
-git
