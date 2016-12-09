0f75af82dea7d35e64dff7d76031c5cccafbe7c8
Кириенков Илья Вадимович <kirienkov@nts.local>
2016-12-09 15:47:34 +0300

    Добавлено задание сравнения строк

---
+++ str_compare.py
@@ -0,0 +1,19 @@
+def str_compare(str1,str2):
+    if str2 == "learn":
+        a=3
+        return (a=3)
+    elif str1 == str2:
+        b=1
+        return b
+    elif str1 != str2  and  len(str1)>len(str2):
+        c=2
+        return c
+
+
+def main():
+    string1 = str(input("Введите строку 1:"))
+    string2 = str(input("Введите строку 2:"))
+    print("Результат сравнения= {}".format(str_compare(string1,string2)))
+
+if __name__ == '__main__':
+    main()
